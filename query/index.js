17ceda7ad4ca7e79ee9e6153b1cca892b2ff6c1e
Erce Can Bektüre <canerce99@gmail.com>
2022-02-08 18:12:09 +0300

    ADD handling missing events by caching them

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -8,14 +9,7 @@ app.use(bodyParser.json());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  console.log(posts);
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
@@ -39,11 +33,32 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+app.get("/posts", (req, res) => {
   console.log(posts);
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
